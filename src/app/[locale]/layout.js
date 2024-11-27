1ac9c53e50d2f3fedfba6e732d29c36bfb0b0cf9
Javad Rafiei <javadrafiei@Javads-MacBook-Air.local>
2024-11-27 15:23:20 +0330

    Configured next-intl folders.

---
+++ src/app/[locale]/layout.js
@@ -0,0 +1,26 @@
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages } from 'next-intl/server';
+import { notFound } from 'next/navigation';
+import { routing } from '@/src/i18n/routing';
+
+export default async function LocaleLayout({ children, params }) {
+  // Ensure that the incoming `locale` is valid
+  const { locale } = await params;
+  if (!routing.locales.includes(locale)) {
+    notFound();
+  }
+
+  // Providing all messages to the client
+  // side is the easiest way to get started
+  const messages = await getMessages();
+
+  return (
+    <html lang={locale}>
+      <body>
+        <NextIntlClientProvider messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
